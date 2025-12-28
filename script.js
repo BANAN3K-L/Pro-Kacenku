cb50d0ad29d626ca297ab1e83fdbd5d5409b1437
Bananek <61901196+BANAN3K-L@users.noreply.github.com>
2025-12-28 11:49:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function part2(){
     var dobaspolu = document.getElementById("doba_spolu");
     var dobaspolu2 = document.getElementById("doba_spolu2");
 
+    var text_button = document.getElementById("text_button");
+
+    var mysicka1 = document.getElementById("text_mysicka");
+    var mysicka2 = document.getElementById("text_mysicka2");
+
     console.log("Přepínám na část 2");
     part1.style.animation = "zavri_p1 1s forwards";
     part2.style.visibility = "visible";
@@ -14,5 +19,15 @@ function part2(){
     dobaspolu.style.animation = "doba_spolu 4s forwards";
     dobaspolu2.style.animation = "doba_spolu2 5s forwards";
 
+    text_button.style.animation = "zobrazeni_tlacitka 5s forwards";
 
+    mysicka1.style.animation = "zobrazeni_mysicek 6s forwards";
+    mysicka2.style.animation = "zobrazeni_mysicek 6.5s forwards";
+}
+
+function part3(){
+    var text_body = document.getElementById("text_body");
+
+    console.log("Spouštím část 3");
+    text_body.style.animation = "roztahnuti_textu 1.5s forwards";
 }
